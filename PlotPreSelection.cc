7a9cf67e1d21d7e5314ec3712f8295121e638dc9
郭佳林 <rogerguo@guojialindeMacBook-Pro.local>
2020-10-25 19:13:43 +0800

    add MC informations

--- PlotPreSelection.cc
+++ PlotPreSelection.cc
@@ -12,11 +12,16 @@ using namespace std;
 //declar some variables
 bool passedZ1LSelection;
 bool passedFullSelection;
-int finalid1=1;
-int finalid2=2;
+bool passedZ4lSelection;
+int final4mu=1;
+int final4e=2;
+int final2mu2e=3;
+int final2e2mu=4;
 Int_t finalState;
+Int_t lep_Hindex[4]; //position of Higgs candidate leptons in lep_p4: 0 = Z1 lead, 1 = Z1 sub, 2 = Z2 lead, 3 = Z2 sub
 std::vector<int> *lep_id;
-std::vector<float> *lep_pt,*lep_eta, *lep_phi, *lep_mass;
+std::vector<float> *lep_pt,*lep_eta, *lep_phi, *lep_mass, *lepFSR_pt, *lepFSR_eta, *lepFSR_phi;
+std::vector<float> *H_pt, *H_eta, *H_phi ,*H_Mass;
 TH1D *Muon_pt = new TH1D("Muon_pt","Muon_pt",50,0,100);
 TH1D *Muon_phi = new TH1D("Muon_phi","Muon_phi",50,-TMath::Pi(),TMath::Pi());
 TH1D *Muon_eta = new TH1D("Muon_eta","Muon_eta",50,-3,3);
@@ -28,6 +33,7 @@ TH1D *Electron_phi = new TH1D("Electron_phi","Electron_phi",50,-TMath::Pi(),TMat
 TH1D *H_mass = new TH1D("H_mass","H_mass",50,70,170);
 TH1D *H_mass_noZinfo = new TH1D("H_mass_noZinfo","H_mass_noZinfo",50,70,170);
 TH1D *Fourlep_mass = new TH1D("4l_mass","4l_mass",1000,70,3000);
+TH1D *H_MassMC = new TH1D("H_MassMC","H_MassMC",50,70,170);
 
 
 //function to set TTress address
@@ -35,21 +41,38 @@ void SetAddressHisto(TTree* t);
 
 void PlotPreSelection(){
   TCanvas* c = new TCanvas();
-//chain all the files
+//chain all the files for signal
   TChain* chain = new TChain("Ana/passedEvents");
-  chain->Add("/pnfs/ihep.ac.cn/data/cms/store/user/guoj/2018data/UFHZZAnalysisRun2/myTask_Data/SingleMuon/crab_SingleMuon_Run2018A-17Sep2018-v2/201018_142705/0000/*.root");
-  chain->Add("/pnfs/ihep.ac.cn/data/cms/store/user/guoj/2018data/UFHZZAnalysisRun2/myTask_Data/SingleMuon/crab_SingleMuon_Run2018A-17Sep2018-v2/201018_142705/0001/*.root");
-  chain->Add("/pnfs/ihep.ac.cn/data/cms/store/user/guoj/2018data/UFHZZAnalysisRun2/myTask_Data/SingleMuon/crab_SingleMuon_Run2018A-17Sep2018-v2/201018_142705/0002/*.root");
-  chain->Add("/pnfs/ihep.ac.cn/data/cms/store/user/guoj/2018data/UFHZZAnalysisRun2/myTask_Data/DoubleMuon/crab_DoubleMuon_Run2018A-17Sep2018-v2/201018_142029/0000/*.root");
-  chain->Add("/pnfs/ihep.ac.cn/data/cms/store/user/guoj/2018data/UFHZZAnalysisRun2/myTask_Data/EGamma/crab_EGamma_Run2018A-17Sep2018-v2/201018_142452/0000/*.root");
-  chain->Add("/pnfs/ihep.ac.cn/data/cms/store/user/guoj/2018data/UFHZZAnalysisRun2/myTask_Data/EGamma/crab_EGamma_Run2018A-17Sep2018-v2/201018_142452/0001/*.root");
-  chain->Add("/pnfs/ihep.ac.cn/data/cms/store/user/guoj/2018data/UFHZZAnalysisRun2/myTask_Data/EGamma/crab_EGamma_Run2018A-17Sep2018-v2/201018_142452/0002/*.root");
-  chain->Add("/pnfs/ihep.ac.cn/data/cms/store/user/guoj/2018data/UFHZZAnalysisRun2/myTask_Data/EGamma/crab_EGamma_Run2018A-17Sep2018-v2/201018_142452/0003/*.root");
-  chain->Add("/pnfs/ihep.ac.cn/data/cms/store/user/guoj/2018data/UFHZZAnalysisRun2/myTask_Data/MuonEG/crab_MuonEG_Run2018A-17Sep2018-v1/201018_142240/0000/*.root");
-  SetAddressHisto(chain);
+  chain->Add("/pnfs/ihep.ac.cn/data/cms/store/user/guoj/2018data/UFHZZAnalysisRun2/myTask_Data/SingleMuon/crab_SingleMuon_Run2018A-17Sep2018-v2/201018_142705/0000/SingleMuon_Run2018A-17Sep2018-v2_19*.root");
+  //chain->Add("/pnfs/ihep.ac.cn/data/cms/store/user/guoj/2018data/UFHZZAnalysisRun2/myTask_Data/SingleMuon/crab_SingleMuon_Run2018A-17Sep2018-v2/201018_142705/0001/*.root");
+  //chain->Add("/pnfs/ihep.ac.cn/data/cms/store/user/guoj/2018data/UFHZZAnalysisRun2/myTask_Data/SingleMuon/crab_SingleMuon_Run2018A-17Sep2018-v2/201018_142705/0002/*.root");
+  //chain->Add("/pnfs/ihep.ac.cn/data/cms/store/user/guoj/2018data/UFHZZAnalysisRun2/myTask_Data/DoubleMuon/crab_DoubleMuon_Run2018A-17Sep2018-v2/201018_142029/0000/*.root");
+  //chain->Add("/pnfs/ihep.ac.cn/data/cms/store/user/guoj/2018data/UFHZZAnalysisRun2/myTask_Data/EGamma/crab_EGamma_Run2018A-17Sep2018-v2/201018_142452/0000/*.root");
+  //chain->Add("/pnfs/ihep.ac.cn/data/cms/store/user/guoj/2018data/UFHZZAnalysisRun2/myTask_Data/EGamma/crab_EGamma_Run2018A-17Sep2018-v2/201018_142452/0001/*.root");
+  //chain->Add("/pnfs/ihep.ac.cn/data/cms/store/user/guoj/2018data/UFHZZAnalysisRun2/myTask_Data/EGamma/crab_EGamma_Run2018A-17Sep2018-v2/201018_142452/0002/*.root");
+  //chain->Add("/pnfs/ihep.ac.cn/data/cms/store/user/guoj/2018data/UFHZZAnalysisRun2/myTask_Data/EGamma/crab_EGamma_Run2018A-17Sep2018-v2/201018_142452/0003/*.root");
+  //chain->Add("/pnfs/ihep.ac.cn/data/cms/store/user/guoj/2018data/UFHZZAnalysisRun2/myTask_Data/MuonEG/crab_MuonEG_Run2018A-17Sep2018-v1/201018_142240/0000/*.root");
+//chain all the files for MC
+  TChain* chainMC = new TChain("Ana/passedEvents");
+  chainMC->Add("/pnfs/ihep.ac.cn/data/cms/store/user/chenguan/VBF_HToZZTo4L_M125_13TeV_powheg2_JHUGenV7011_pythia8/crab_2018VBF_125/200510_155950/0000/2018VBF_125_13.root");
 
-  cout<<chain->GetEntries()<<endl;
-// loop over the TFiles that have been added to this chain
+//loop over the MC TFiles that have been added to this chain
+  SetAddressHisto(chainMC);
+  for(int i=0;i<chainMC->GetEntries(); i++){
+    chainMC->GetEntry(i);
+    if(passedZ4lSelection!=1) continue;
+    if(passedFullSelection==1&&(finalState==final4mu||finalState==final4e||finalState==final2mu2e||final2e2mu)){
+      for(int j=0; j<H_pt->size(); j++){
+        TLorentzVector H_massMC;
+        H_massMC.SetPtEtaPhiM((*H_pt)[j],(*H_eta)[j],(*H_phi)[j],(*H_Mass)[j]);
+        H_MassMC->Fill(H_massMC.M());
+      }
+    }
+  }
+
+
+// loop over the signal TFiles that have been added to this chain
+  SetAddressHisto(chain);
   for(int i=0;i<chain->GetEntries(); i++){
       chain->GetEntry(i);
       for(int j=0; j<lep_id->size(); j++){
@@ -76,17 +99,17 @@ void PlotPreSelection(){
       lep4_noZ.SetPtEtaPhiM((*lep_pt)[3],(*lep_eta)[3],(*lep_phi)[3],(*lep_mass)[3]);
       TLorentzVector Higgs_noZ = lep1_noZ+lep2_noZ+lep3_noZ+lep4_noZ;
       H_mass_noZinfo->Fill(Higgs_noZ.M());
+      Fourlep_mass->Fill(Higgs.M());
 
-      if(passedZ1LSelection==!0) continue;
-      if(passedFullSelection==1&&(finalState==finalid1||finalState==finalid2)){
+      if(passedZ4lSelection!=1) continue;
+      if(passedFullSelection==1&&(finalState==final4mu||finalState==final4e||finalState==final2mu2e||final2e2mu)){
       TLorentzVector lep1, lep2, lep3, lep4;
-      lep1.SetPtEtaPhiM((*lep_pt)[0],(*lep_eta)[0],(*lep_phi)[0],(*lep_mass)[0]);
-      lep2.SetPtEtaPhiM((*lep_pt)[1],(*lep_eta)[1],(*lep_phi)[1],(*lep_mass)[1]);
-      lep3.SetPtEtaPhiM((*lep_pt)[2],(*lep_eta)[2],(*lep_phi)[2],(*lep_mass)[2]);
-      lep4.SetPtEtaPhiM((*lep_pt)[3],(*lep_eta)[3],(*lep_phi)[3],(*lep_mass)[3]);
+      lep1.SetPtEtaPhiM((*lepFSR_pt)[lep_Hindex[0]],(*lepFSR_pt_eta)[lepFSR_pt_Hindex[0]],(*lep_phi)[lep_Hindex[0]],(*lep_mass)[lep_Hindex[0]]);
+      lep2.SetPtEtaPhiM((*lepFSR_pt)[lep_Hindex[1]],(*lepFSR_eta)[1],(*lepFSR_phi)[lep_Hindex[1]],(*lep_mass)[lep_Hindex[1]]);
+      lep3.SetPtEtaPhiM((*lep_pt)[lep_Hindex[2]],(*lep_eta)[2],(*lep_phi)[lep_Hindex[2]],(*lep_mass)[lep_Hindex[2]]);
+      lep4.SetPtEtaPhiM((*lep_pt)[lep_Hindex[3]],(*lep_eta)[lep_Hindex[2]],(*lep_phi)[lep_Hindex[2]],(*lep_mass)[lep_Hindex[2]]);
       TLorentzVector Higgs=lep1+lep2+lep3+lep4;
       H_mass->Fill(Higgs.M());
-      Fourlep_mass->Fill(Higgs.M());
     }
   }
       Muon_pt->Draw();  c->SaveAs("Mu_pt.png");
@@ -96,7 +119,8 @@ void PlotPreSelection(){
       Electron_pt->Draw(); c->SaveAs("e_pt.png");
       Electron_eta->Draw(); c->SaveAs("e_eta.png");
       Electron_phi->Draw(); c->SaveAs("e_phi.png");
-      H_mass->Draw(); c->SaveAs("H_mass.png");
+      H_MassMC->SetFillColor(KRed); H_MassMC->Draw();
+      H_mass->GetYaxis()->SetTitle("Events / 2 GeV"); H_mass->Draw("sameE1"); c->SaveAs("H_mass.png");
       H_mass_noZinfo->Draw(); c->SaveAs("H_mass_noZinfo.png");
       Fourlep_mass->Draw(); c->SaveAs("4l_mass.png");
 }
@@ -104,6 +128,7 @@ void PlotPreSelection(){
 
 //define function
 void SetAddressHisto(TTree* t){
+  t->SetBranchAddress("lep_Hindex",&lep_Hindex);
   t->SetBranchAddress("lep_id",&lep_id);
   t->SetBranchAddress("lep_pt",&lep_pt);
   t->SetBranchAddress("lep_eta",&lep_eta);
@@ -111,5 +136,12 @@ void SetAddressHisto(TTree* t){
   t->SetBranchAddress("lep_mass",&lep_mass);
   t->SetBranchAddress("passedZ1LSelection",&passedZ1LSelection);
   t->SetBranchAddress("passedFullSelection",&passedFullSelection);
+  t->SetBranchAddress("passedZ4lSelection",&passedZ4lSelection);
   t->SetBranchAddress("finalState",&finalState);
+  t->SetBranchAddress("lepFSR_pt",&lepFSR_pt);
+  t->SetBranchAddress("lepFSR_eta",&lepFSR_eta);
+  t->SetBranchAddress("H_pt",&H_pt);
+  t->SetBranchAddress("H_eta",&H_eta);
+  t->SetBranchAddress("H_phi",&H_phi);
+  t->SetBranchAddress("H_Mass",&H_mass);
 }
