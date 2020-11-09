f23f3f82baae0578f2e340ff371f0c4cf6103712
郭佳林 <rogerguo@guojialindeMacBook-Pro.local>
2020-11-09 13:51:15 +0800

    11111

--- Compare.py
+++ Compare.py
@@ -32,12 +32,12 @@ Sim.SetFillColor(ROOT.kRed)
 
 Data = ROOT.TH1D("Data","Data",50,70,170)
 Data.GetYaxis().SetTitle("Events / 2 GeV")
-Data.SetMarkerStyle(20);
-Data.SetMarkerColor(ROOT.kBlack);
-Data.SetMarkerSize(1.2);
-Data.SetLineColor(ROOT.kBlack);
-Data.SetLineWidth(1);
-Data.SetStats(ROOT.kFALSE);
+Data.SetMarkerStyle(20)
+Data.SetMarkerColor(ROOT.kBlack)
+Data.SetMarkerSize(1.2)
+Data.SetLineColor(ROOT.kBlack)
+Data.SetLineWidth(1)
+Data.SetStats(ROOT.kFALSE)
 
 leg = ROOT.TLegend(0.7, 0.7, 0.85, 0.85)
 leg.AddEntry(Data,"Data","PE1")
@@ -50,7 +50,7 @@ leg.SetLineColor(10)
 
 #Loop over all the events and fill histogram
 for ievent,event in enumerate(ggTozz):
-    gg.Fill(event.H_FSR)
+    gg.Fill(event.H_FSR,(35.9*1000/gg.Integral())*event.eventWeight)
 
 for ievent,event in enumerate(qqTozz):
     qq.Fill(event.H_FSR)
@@ -61,17 +61,12 @@ for ievent,event in enumerate(DataSim):
 for ievent,event in enumerate(t):
     Data.Fill(event.H_FSR)
 
-#normalize
-lw = 0.359*1000
-gg.Scale(lw/gg.Integral())
-qq.Scale(lw/qq.Integral())
-Sim.Scale(lw/Sim.Integral())
+
 #set histo and drew
 hstack = ROOT.THStack("hstack","2016reuslt")
 hstack.Add(gg)
 hstack.Add(qq)
 hstack.Add(Sim)
-hstack.GetYaxis().SetTitle("Events / 2 GeV")
 hstack.Draw("histo")
 Data.Draw("sameE1")
 leg.Draw()
