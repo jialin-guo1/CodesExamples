c98053e278f818878d9f54322c8720a67c0e7544
jialin-guo1 <jialin.guo@cern.ch>
2020-11-18 15:26:15 +0800

    zx 11

--- ZplusX.py
+++ ZplusX.py
@@ -1,24 +1,24 @@
 import ROOT
 
-t = ROOT.TChain("passedEvents")
-t.Add("/afs/cern.ch/work/g/guoj/XToZZ_FullRunII/Data2016/2016_noDuplicates_new.root")
+Signal = ROOT.TFile('/afs/cern.ch/work/g/guoj/XToZZ_FullRunII/Data2016/2016_allsignal_new.root')
+t = Signal.Get('passedEvents')
 
 #Z+X
 c = ROOT.TCanvas()
 
-ZX1 = ROOT.TH1D("ZX1","ZX1",10,0,1)
+ZX1 = ROOT.TH1D("ZX1","ZX1",10,0.3,1)
 ZX1.GetYaxis().SetTitle("Events / 2 GeV")
 ZX1.SetLineColor(ROOT.kRed)
 
-ZX2 = ROOT.TH1D("ZX2","ZX2",10,0,1)
+ZX2 = ROOT.TH1D("ZX2","ZX2",10,0.3,1)
 ZX2.GetYaxis().SetTitle("Events / 2 GeV")
 ZX2.SetLineColor(ROOT.kBlue)
 
-ZX3 = ROOT.TH1D("ZX3","ZX3",10,0,1)
+ZX3 = ROOT.TH1D("ZX3","ZX3",10,0.3,1)
 ZX3.GetYaxis().SetTitle("Events / 2 GeV")
 ZX3.SetLineColor(ROOT.kBlack)
 
-ZX4 = ROOT.TH1D("ZX4","ZX4",10,0,1)
+ZX4 = ROOT.TH1D("ZX4","ZX4",10,0.3,1)
 ZX4.GetYaxis().SetTitle("Events / 2 GeV")
 ZX4.SetLineColor(7)
 
@@ -32,4 +32,4 @@ ZX1.Draw()
 ZX2.Draw("same")
 ZX3.Draw("same")
 ZX4.Draw("same")
-c.SaveAs("ZX.png")
+c.SaveAs("ZX01.png")
