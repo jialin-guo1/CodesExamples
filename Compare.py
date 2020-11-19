f2c39d484ea4081b4b63ff1cc7865a428e0292bb
jialin-guo1 <jialin.guo@cern.ch>
2020-11-19 13:47:08 +0800

    all 1

--- Compare.py
+++ Compare.py
@@ -144,7 +144,7 @@ Sim_ZH.GetYaxis().SetTitle("Events / 2 GeV")
 
 
 #Data = ROOT.TH1D("Data","Data(2016)",50,70,170)
-Data = ROOT.TH1D("Data","result(2017)",50,70,170)
+Data = ROOT.TH1D("Data","result(RuII)",50,70,170)
 Data.GetYaxis().SetTitle("Events / 2 GeV")
 Data.SetMarkerStyle(20)
 Data.SetMarkerColor(ROOT.kBlack)
